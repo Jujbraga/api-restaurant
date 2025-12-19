7c0a742618b82f1d2b7126940c9241cd3f0ea028
Juliana Braga <jujbraga@gmail.com>
2025-12-19 09:55:28 +0100

    install zod and create a zod validation

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import { ZodError } from "zod";
 
 import { AppError } from "@/utils/AppError";
 
@@ -10,6 +11,12 @@ export function errorHandling(
 ) {
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message });
+  }
+
+  if (error instanceof ZodError) {
+    return response
+      .status(400)
+      .json({ message: "validation error", issues: error.format() });
   }
 
   return response.status(500).json({ message: error.message });
