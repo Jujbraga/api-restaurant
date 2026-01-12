7d2d28ce46dba640677a13c3c61962cb3e6492ce
Juliana Braga <jujbraga@gmail.com>
2026-01-12 11:55:32 +0100

    put method to uptade a product

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { knex } from "@/database/knex";
-import { z } from "zod";
+import { number, z } from "zod";
 
 class ProductController {
   async index(requst: Request, response: Response, next: NextFunction) {
@@ -29,6 +29,31 @@ class ProductController {
       await knex<ProductRepository>("products").insert({ name, price });
 
       return response.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update(request: Request, response: Response, next: NextFunction) {
+    try {
+      const id = z
+        .string()
+        .transform((value) => Number(value))
+        .refine((value) => !isNaN(value), { message: "id must be a number" })
+        .parse(request.params.id);
+
+      const bodySchema = z.object({
+        name: z.string().trim().min(4),
+        price: z.number().gt(0),
+      });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      await knex<ProductRepository>("products")
+        .update({ name, price, updated_at: knex.fn.now() })
+        .where({ id });
+
+      return response.json();
     } catch (error) {
       next(error);
     }
