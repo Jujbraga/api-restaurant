60ac86328d43a40cedc285b70c4715cc294a66e6
Juliana Braga <jujbraga@gmail.com>
2026-01-15 14:17:55 +0100

    post method in orders with validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import { Router } from "express";
 import { productsRoutes } from "./products-route";
 import { tablesRoutes } from "./tables-routes";
 import { tablesSessionsRoutes } from "./tables-sessions-routes";
+import { ordersRoutes } from "./orders-route";
 
 const routes = Router();
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
 routes.use("/tables-sessions", tablesSessionsRoutes);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
