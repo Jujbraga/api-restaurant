f4ef2cccc64f40882b87cffc61f976e5f5a13225
Juliana Braga <jujbraga@gmail.com>
2026-01-15 11:08:13 +0100

    post method (create) in tables sessions

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { knex } from "@/database/knex";
+import { AppError } from "@/utils/AppError";
 
 class TablesSessionsController {
   async create(request: Request, response: Response, next: NextFunction) {
     try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      });
+
+      const { table_id } = bodySchema.parse(request.body);
+
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ table_id })
+        .orderBy("opened_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("This table is already open");
+      }
+
+      await knex<TablesSessionsRepository>("tables_sessions").insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
       response.status(201).json();
     } catch (error) {
       next(error);
